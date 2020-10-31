0f14e8db0336ed21b30aacdf8a7fe827313c205d
hongrae.jin <k18lovean@naver.com>
2020-10-31 21:51:13 +0900

    LabelFrame content add     - Layout     - Layout Padding     - Widget Padding

--- src/2장 레이아웃 관리/GUI_LabelFrame_column_one.py
+++ src/2장 레이아웃 관리/GUI_LabelFrame_column_one.py
@@ -5,8 +5,8 @@ from tkinter import ttk
 win = tk.Tk()
 win.title("Raymond Application")
 
-buttons_frame = ttk.LabelFrame(win, text="Labels in a Frame")
-buttons_frame.grid(column=0, row=0)
+buttons_frame = ttk.LabelFrame(win, text="")
+buttons_frame.grid(column=0, row=0, padx=20, pady=40)
 
 for i in range(3):
     ttk.Label(buttons_frame, text='Label'+str(i)).grid(column=i, row=0, sticky=tk.W)
@@ -14,5 +14,7 @@ for i in range(3):
 for i in range(3):
     ttk.Label(buttons_frame, text='Label'+str(i)).grid(column=3, row=i , sticky=tk.W)
 
+for child in buttons_frame.winfo_children():
+    child.grid_configure(padx=8, pady=4)
 
 win.mainloop()
